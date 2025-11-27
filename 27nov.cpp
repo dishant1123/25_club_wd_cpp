3ce2731dc3468e029d76667e9a9344197a8bfab3
dishant1123 <dishantshah1417@gmail.com>
2025-11-27 10:59:47 +0530

    ...

---
+++ 27nov.cpp
@@ -0,0 +1,44 @@
+// #include<iostream>
+// int main()
+// {
+//     std :: cout<<"hello world";
+//     return 0;
+// }
+
+// #include<iostream>
+// using namespace std; 
+// int main()
+// {
+//     cout<<"hello world"<<endl;
+//     cout<<"raj";
+    
+//     return 0;
+// }
+
+/*
+int   @q =90
+int a12 =90
+int _=89 
+*/
+
+/*
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a__=90; 
+    cout<<"value  is  :"<<a__<<endl;
+    return 0;
+}
+*/ 
+
+#include<iostream>
+using namespace std; 
+int main()
+{
+    float a=12.34; 
+    cout<<"value  is  :"<<a<<endl;
+    return 0; 
+}
+
+
